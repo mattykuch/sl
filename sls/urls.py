00cc36d3f51a9e4178c7aaf76ec9ff3299f90126
Matthew Kuch <kuch.matthew@gmail.com>
2016-09-17 17:47:52 +0300

    Functionality of pages-optimal

--- sls/urls.py
+++ sls/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 
 	# Show all vaccines
-	url(r'^vaccines/$', views.vaccines, name='vaccines')
+	url(r'^vaccines/$', views.vaccines, name='vaccines'),
+
+	# Detail page for a single vaccine
+	url(r'^vaccines/(?P<vaccine_id>\d+)/$', views.vaccine, name='vaccine'),
 ]
--- sls/views.py
+++ sls/views.py
@@ -11,3 +11,10 @@ def vaccines(request):
 	vaccines = Vaccine.objects.order_by('date_added')
 	context = {'vaccines': vaccines}
 	return render(request, 'sls/vaccines.html', context)
+
+def vaccine(request, vaccine_id):
+	"""Show a single vaccine and all its entries."""
+	vaccine = Vaccine.objects.get(id=vaccine_id)
+	entries = vaccine.entry_set.order_by('id')
+	context = { 'vaccine': vaccine, 'entries': entries}
+	return render(request, 'sls/vaccine.html', context)
